fbaf92574661749908a2f4d85495ac86b5f14544
iaSalazar <ia.salazar@uniandes.edu.co>
2023-03-09 16:52:45 -0500

    minnor fix

--- api/utils/loadUserDb.py
+++ api/utils/loadUserDb.py
@@ -32,10 +32,9 @@ def callApi(row):
 
 all_rows = len(df)
 for i in range(all_rows):
-    # choose the first half of cols before "activities"
+
     row_dict = dict(df.iloc[i])
-    # add the "activites" with the rest of the cols
-    # dict has single quotes which throws an error if sent to an API, so use json.dumps
+
     row_dict = json.dumps(row_dict)
     print(row_dict)
     callApi(row_dict)
