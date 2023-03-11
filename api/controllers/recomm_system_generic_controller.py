e94d2160f7013bfd6113de171dc0b5a80a789c0e
iaSalazar <ia.salazar@uniandes.edu.co>
2023-03-11 18:26:50 -0500

    generic recommendations back

---
+++ api/controllers/recomm_system_generic_controller.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pickle
+from flask_restful import Resource
+import pandas as pd
+from ..app.extensions import db, api, guard
+from flask import request
+from flask_restful import Resource
+import flask_praetorian
+from sklearn.neighbors import NearestNeighbors
+from flask import Response
+
+
+class ResourceGenericRecommendations(Resource):
+    @flask_praetorian.auth_required
+    def get(self):
+
+        ratings = pd.read_csv(
+            '/home/ivs/Documents/MyProjects/SDR/sdr-taller-1/api/data/preprocessed_user_item_rating.csv')
+        agg_ratings = ratings.groupby('artist-name').agg(mean_rating=('rating', 'mean'),
+                                                         number_of_ratings=('rating', 'count')).reset_index().sort_values('number_of_ratings',  ascending=False)
+        agg_ratings = agg_ratings[agg_ratings['number_of_ratings'] > 10]
+
+        agg_ratings = agg_ratings.sort_values(
+            ['number_of_ratings', 'mean_rating'],  ascending=False)[:10]
+
+        return Response(agg_ratings.to_json(orient="records"), mimetype='application/json')
+
+
+api.add_resource(ResourceGenericRecommendations,
+                 '/api/recommendations/generic')
